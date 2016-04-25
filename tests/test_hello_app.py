8f52e5f219fcf981c7b0dadda0e79a319ab4f9e0
lenarother <rother.magdalena@gmail.com>
2016-04-25 22:22:58 +0200

    Add failing acceptance test for hello world django application on localhost

---
+++ tests/test_hello_app.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from unittest import TestCase, main
+
+
+class HelloWorldAppTests(TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def tearDown(self):
+        self.driver.close()
+
+    def test_body(self):
+        self.driver.get("http://127.0.0.1:8000")
+        self.assertTrue('Hello world!' in self.driver.page_source)
+
+if __name__ == "__main__":
+    main()
