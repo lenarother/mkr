5c9e775c2b4b032a07b95cc7f73abf7462b98449
lenarother <rother.magdalena@gmail.com>
2016-05-02 00:12:39 +0200

    Read quote from database in views

--- docker/hello_world/hello/views.py
+++ docker/hello_world/hello/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from models import Quotes
 
 def hello(request):
     return render(request, 'hello/hello.html', {})
 
 def quote(request):
     """Requests a quote from the database and displays it."""
-    quote = "bla blablabla bla bla, bla blabla bla (B. Bla)"
-    return render(request, 'hello/quote.html', {"quote": quote})
+    quote = Quotes.objects.first()
+    return render(request, 'hello/quote.html', {"quote": str(quote) })
