a5775c07a662df774187255846aa46560397d516
lenarother <rother.magdalena@gmail.com>
2016-05-01 23:45:02 +0200

    Add initial migration with a single table Quotes

--- docker/hello_world/hello/models.py
+++ docker/hello_world/hello/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Quotes(models.Model):
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+
+    def __str__(self):
+        return '{} says {}'.format(self.author, self.text)
