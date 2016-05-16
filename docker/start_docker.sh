724722ca46d853d599fd2e1fe6246f6255bdbfad
krother <krother@academis.eu>
2016-05-16 22:33:34 +0200

    docker-compose startup process in separate shell script

--- docker/start_docker.sh
+++ docker/start_docker.sh
@@ -1,7 +1,11 @@
 
+# stop and remove everything
+docker-compose stop
+docker ps -a | awk '{print $1}' | xargs --no-run-if-empty docker rm
 
+# start the system
+docker-compose create
+docker-compose start db
 docker-compose run -w /code/hello_world django python manage.py migrate
-
-docker-compose run -w /code/hello_world django python manage.py createsuperuser --username root --email root@testsrv.er 
-
-docker-compose up
+docker-compose start django
+docker-compose logs -f
