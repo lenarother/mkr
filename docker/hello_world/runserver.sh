46f3450248c623b86ce067ce8c4d23ee240433e0
lenarother <rother.magdalena@gmail.com>
2016-05-16 23:37:50 +0200

    Fix psql service health check

--- docker/hello_world/runserver.sh
+++ docker/hello_world/runserver.sh
@@ -6,12 +6,13 @@
 # from: https://howchoo.com/g/y2y1mtkznda/getting-started-with-docker-compose-and-django
 
 # wait for mysql to be ready
-nc -z mysql 3306
+nc -z db 5432
 n=$?
 while [ $n -ne 0 ]; do
     sleep 1
-    nc -z mysql 3306
+    nc -z db 5432
     n=$?
 done
 
-python manage.py runserver 0.0.0.0:8000
+python /code/hello_world/manage.py migrate
+python /code/hello_world/manage.py runserver 0.0.0.0:8000
