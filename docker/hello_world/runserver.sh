e6e928e5a72922dac9b4dba1589826f069490688
lenarother <rother.magdalena@gmail.com>
2016-05-24 22:29:19 +0200

    Extract postgres health check to separate file

--- docker/hello_world/runserver.sh
+++ docker/hello_world/runserver.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-# Service Health Check
-
-# script to check DB availability before starting django
-# from: https://howchoo.com/g/y2y1mtkznda/getting-started-with-docker-compose-and-django
-
-# wait for postgres to be ready
-nc -z db 5432
-n=$?
-while [ $n -ne 0 ]; do
-    sleep 1
-    nc -z db 5432
-    n=$?
-done
+# check whether postgres is ready
+bash wait_for_db.sh
 
 python /code/hello_world/manage.py migrate
 python /code/hello_world/manage.py runserver 0.0.0.0:8000
