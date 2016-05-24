334c500faf551e46935855b98903ecf85b4c801a
lenarother <rother.magdalena@gmail.com>
2016-05-24 22:20:14 +0200

    Clean

--- docker/hello_world/hello/tests.py
+++ docker/hello_world/hello/tests.py
@@ -5,19 +5,19 @@ from models import Quotes
 # Create your tests here.
 class TestHello(TestCase):
 
-	@classmethod
-	def setUpTestData(cls):
-		q = Quotes(author="Sun Tzu", text="The best warfare strategy is to attack the enemy's plans.")
-		q.save()
+    @classmethod
+    def setUpTestData(cls):
+        q = Quotes(author="Sun Tzu", text="The best warfare strategy is to attack the enemy's plans.")
+        q.save()
 
-	def test_hello(self):
-		client = Client()
-		response = client.get('/')
-		self.assertEqual(response.status_code, 200)
-		self.assertIn("Hello World!", response.content)
+    def test_hello(self):
+        client = Client()
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("Hello World!", response.content)
 
-	def test_quotes(self):
-		client = Client()
-		response = client.get('/quote/')
-		self.assertEqual(response.status_code, 200)
-		self.assertIn("The best warfare strategy", response.content)
+    def test_quotes(self):
+        client = Client()
+        response = client.get('/quote/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("The best warfare strategy", response.content)
