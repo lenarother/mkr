dac4ca8650ea0e9dc34ae632a8040f15176fab73
krother <krother@academis.eu>
2016-05-24 22:19:44 +0200

    identified defect in startup process

--- docker/hello_world/runserver.sh
+++ docker/hello_world/runserver.sh
@@ -5,7 +5,7 @@
 # script to check DB availability before starting django
 # from: https://howchoo.com/g/y2y1mtkznda/getting-started-with-docker-compose-and-django
 
-# wait for mysql to be ready
+# wait for postgres to be ready
 nc -z db 5432
 n=$?
 while [ $n -ne 0 ]; do
