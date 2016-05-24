1be87f2551669e11b49d3ba759c04e5487c1af80
lenarother <rother.magdalena@gmail.com>
2016-05-24 23:39:57 +0200

    Add management command for loading quotes

---
+++ docker/hello_world/hello/management/commands/load_quotes.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+
+from hello.parse_quotes import parse_quotes
+from hello.models import Quotes
+
+class Command(BaseCommand):
+
+	help = "Loads quote from txt file to database"
+
+	def add_arguments(self, parser):
+		parser.add_argument("--file_name",
+			dest="file_name",
+			required=True,
+			help="Path to the file containing quotes")
+
+	def handle(self, *args, **options):
+		quotes = parse_quotes(options['file_name'])
+		for author, quote in quotes:
+			new_quote = Quotes(author=author, text=quote)
+			new_quote.save()
