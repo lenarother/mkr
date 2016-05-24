b3baf66ff22c787adb7f838255474362e54ad01f
lenarother <rother.magdalena@gmail.com>
2016-05-25 00:22:45 +0200

    Clean

--- docker/hello_world/hello/management/commands/load_quotes.py
+++ docker/hello_world/hello/management/commands/load_quotes.py
@@ -5,16 +5,16 @@ from hello.models import Quotes
 
 class Command(BaseCommand):
 
-	help = "Loads quote from txt file to database"
+    help = "Loads quote from txt file to database"
 
-	def add_arguments(self, parser):
-		parser.add_argument("--file_name",
-			dest="file_name",
-			required=True,
-			help="Path to the file containing quotes")
+    def add_arguments(self, parser):
+        parser.add_argument("--file_name",
+            dest="file_name",
+            required=True,
+            help="Path to the file containing quotes")
 
-	def handle(self, *args, **options):
-		quotes = parse_quotes(options['file_name'])
-		for author, quote in quotes:
-			new_quote = Quotes(author=author, text=quote)
-			new_quote.save()
+    def handle(self, *args, **options):
+        quotes = parse_quotes(options['file_name'])
+        for author, quote in quotes:
+            new_quote = Quotes(author=author, text=quote)
+            new_quote.save()
