36e8963d3c517e9b1959593b4ffeeec2c1acb8ab
lenarother <rother.magdalena@gmail.com>
2016-05-25 23:30:20 +0200

    Add quotes source file for acceptance test

--- docker/fabfile.py
+++ docker/fabfile.py
@@ -1,8 +1,17 @@
 
 import os
 
-def test():
-    os.system('docker-compose run -w /code/hello_world/ django python manage.py test')
+def bash():
+    os.system('docker-compose run django bash')
+
+def freeze():
+    os.system('docker-compose run -w /code/hello_world/ django pip freeze')
 
 def load():
     os.system('docker-compose run -w /code/hello_world/ django python manage.py load_quotes --file_name ../quotes.txt')
+
+def shell():
+    os.system('docker-compose run -w /code/hello_world/ django python manage.py shell')
+
+def test():
+    os.system('docker-compose run -w /code/hello_world/ django python manage.py test')
